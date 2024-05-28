f33071af056059af9987f061ba4df859dc7bea16
Ronmi Ren <ronmi.ren@gmail.com>
2024-05-28 19:15:26 +0800

    handler receieves context as param, preventing copying request

--- handler.go
+++ handler.go
@@ -5,8 +5,11 @@
 package jsonapi
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -15,46 +18,55 @@ import (
 // Just return something, and it will be encoded to JSON format and send to client.
 // Or return an Error to specify http status code and error string.
 //
-//     func myHandler(dec *json.Decoder, httpData *HTTP) (interface{}, error) {
-//         var param paramType
-//         if err := dec.Decode(&param); err != nil {
-//             return nil, jsonapi.E400.SetData("You must send parameters in JSON format.")
-//         }
-//         return doSomething(param), nil
-//     }
+//	func myHandler(ctx context.Context, req *Request) (interface{}, error) {
+//	    var param paramType
+//	    if err := req.Decode(&param); err != nil {
+//	        return nil, jsonapi.E400.SetData("You must send parameters in JSON format.")
+//	    }
+//	    return doSomething(param), nil
+//	}
+//
+// The context is derived from [http.Request.Context]. You should check if it is
+// canceled before doing time/resource consuming job in general. There's a helper
+// [IsCanceled].
 //
 // To redirect clients, return 301~303 status code and set Data property
 //
-//     return nil, jsonapi.E301.SetData("http://google.com")
+//	return nil, jsonapi.E301.SetData("http://google.com")
 //
 // Redirecting depends on http.Redirect(). The data returned from handler will never
 // write to ResponseWriter.
 //
 // This basically obey the http://jsonapi.org rules:
 //
-//     - Return {"data": your_data} if error == nil
-//     - Return {"errors": [{"code": application-defined-error-code, "detail": message}]} if error returned
-type Handler func(r Request) (interface{}, error)
+//   - Return {"data": your_data} if error == nil
+//   - Return {"errors": [{"code": "error-code", "detail": "message"}]} if error returned
+type Handler func(ctx context.Context, r Request) (interface{}, error)
 
 // ServeHTTP implements net/http.Handler
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	enc := json.NewEncoder(w)
-	res, err := h(FromHTTP(w, r))
-	resp := make(map[string]interface{})
-	if err == nil {
-		resp["data"] = res
-		e := enc.Encode(resp)
-		if e == nil {
-			return
-		}
-		delete(resp, "data")
+	defer r.Body.Close()
+	defer io.Copy(io.Discard, r.Body)
 
-		err = E500.SetOrigin(e).SetData(
-			`Failed to marshal data`,
-		)
+	w.Header().Set("Content-Type", "application/json")
+	res, err := h(r.Context(), FromHTTP(w, r))
+	if IsCanceled(r.Context()) != nil {
+		// connection is closed, do not output anything
+		return
 	}
 
+	enc := json.NewEncoder(w)
+	if err == nil {
+		err = enc.Encode(map[string]interface{}{
+			"data": res,
+		})
+		if err == nil {
+			return
+		}
+		err = errors.New("Failed to marshal data")
+	}
+
+	resp := make(map[string]interface{})
 	code := http.StatusInternalServerError
 	if httperr, ok := err.(Error); ok {
 		if httperr.EqualTo(ASIS) {
