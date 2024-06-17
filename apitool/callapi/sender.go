daf8ea2ede51026d9170aebd48de3bb3a8d351ed
Ronmi Ren <ronmi.ren@gmail.com>
2024-06-17 12:27:41 +0800

    Sender.ParseWith accepts default value

--- apitool/callapi/sender.go
+++ apitool/callapi/sender.go
@@ -12,7 +12,8 @@ import (
 // Sender is s function to send request to server.
 type Sender func(ctx context.Context, param any) (resp *http.Response, err error)
 
-// ParseWith creates a Caller by parsing response returned by Sender s.
+// ParseWith creates a Caller by parsing response returned by Sender s. It uses
+// [DefaultParser] if p == nil.
 func (s Sender) ParseWith(p Parser) Caller {
 	return func(ctx context.Context, param, result any) (err error) {
 		resp, err := s(ctx, param)
@@ -20,6 +21,9 @@ func (s Sender) ParseWith(p Parser) Caller {
 			return
 		}
 
+		if p == nil {
+			p = DefaultParser
+		}
 		return p(resp, result)
 	}
 }
