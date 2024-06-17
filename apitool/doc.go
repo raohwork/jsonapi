26bb7041b7e04bafaaafecf149005e2dad7c653d
Ronmi Ren <ronmi.ren@gmail.com>
2024-06-17 14:15:47 +0800

    add disclaimer

--- apitool/doc.go
+++ apitool/doc.go
@@ -2,5 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Package apitool provides few middlewares helping you create your app
+// Package apitool provides few middlewares helping you create your app.
+//
+// Tools in this package and sub-directories are opinionated, should be good to be
+// used in small project. But it's suggested to write your own for large project.
 package apitool
